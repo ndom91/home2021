63f3e23d3262c71136a8beb0ff66b78b16c8c77f
ndom91 <yo@ndo.dev>
2021-06-22 02:06:07 +0200

    fix: add VERCEL_URL

--- api/og.ts
+++ api/og.ts
@@ -24,6 +24,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   await page.goto(url, {
     timeout: 15 * 1000,
   })
+  if (req?.query?.path === "/") {
+    await page.waitForTimeout(2000)
+  }
   const data = await page.screenshot({
     type: "png",
   })
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 export const getAbsoluteURL = (path: string | string[]) => {
-  const baseURL = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
+  const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : "http://localhost:3003"
   return baseURL + path
 }
