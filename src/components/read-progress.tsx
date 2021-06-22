af5a9701f224ae27114e576cdbfc1aa73bfef37d
ndom91 <yo@ndo.dev>
2021-06-22 04:39:56 +0200

    feat: tailwind read-progress bar

--- src/components/read-progress.tsx
+++ src/components/read-progress.tsx
@@ -3,28 +3,23 @@ import { useEffect } from "react"
 
 const ProgressBar = () => {
   const { y } = useWindowScroll()
-  // const [scrollPos, setScrollPos] = useState(0)
   const pageHeight = document.body.scrollHeight - window.innerHeight
 
   useEffect(() => {
-    // console.log(verticalScrollOffset, scrollTop)
     const percentScrolled = Math.abs(y) / pageHeight
     const el = document.getElementById("scroll-progress")
     if (el) {
-      el.style.background = `
-        linear-gradient(
-          to right,
-          var(--tw-ring-color) ${percentScrolled * 100}%,
-          transparent 0
-        )
-      `
+      el.style.setProperty(
+        "--tw-gradient-stops",
+        `#f9a8d4 ${percentScrolled * 100}%, transparent 0`
+      )
     }
   }, [y, pageHeight])
 
   return (
     <div
       id="scroll-progress"
-      className="fixed top-0 left-0 z-50 w-full h-2 p-0 ring-pink-300 dark:ring-palevioletred"
+      className="fixed top-0 left-0 z-50 w-full h-2 p-0 bg-gradient-to-r from-pink-300 to-transparent via-transparent ring-pink-300 dark:ring-palevioletred"
     />
   )
 }
