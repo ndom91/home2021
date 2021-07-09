540072a839b0ac91eb17464cd7fbeb04660a3b38
ndom91 <yo@ndo.dev>
2021-07-09 23:26:08 +0200

    style: nav animations

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -10,21 +10,21 @@ const Navbar = () => {
       <ul className="flex items-center w-full space-x-10 text-xl text-gray-800 dark:text-gray-100 font-extralight">
         <li>
           <Link href="/">
-            <a className="p-0.5 dark:text-gray-100 text-gray-800 font-light hover:border-b-8 focus:border-b-8 dark:border-palevioletred border-pink-300 focus:outline-none outline-none transition-all duration-200">
+            <a className="p-0.5 dark:text-gray-100 text-gray-800 font-light  focus:outline-none outline-none transition-all duration-200">
               home
             </a>
           </Link>
         </li>
         <li>
           <Link href="/blog">
-            <a className="p-0.5 dark:text-gray-100 text-gray-800 font-light hover:border-b-8 focus:border-b-8 dark:border-palevioletred border-pink-300 focus:outline-none outline-none transition-all duration-200">
+            <a className="p-0.5 dark:text-gray-100 text-gray-800 font-light  focus:outline-none outline-none transition-all duration-200">
               blog
             </a>
           </Link>
         </li>
         <li>
           <Link href="/projects">
-            <a className="p-0.5 dark:text-gray-100 text-gray-800 font-light hover:border-b-8 focus:border-b-8 dark:border-palevioletred border-pink-300 focus:outline-none outline-none transition-all duration-200">
+            <a className="p-0.5 dark:text-gray-100 text-gray-800 font-light  focus:outline-none outline-none transition-all duration-200">
               work
             </a>
           </Link>
