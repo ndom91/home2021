37b204d1858a24f03d9d9a826b9e6d7be349f378
ndom91 <yo@ndo.dev>
2021-07-20 23:49:21 +0200

    fix: 404 style tweaks :art:

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,19 +7,21 @@ const FourOhFour = () => {
   const router = useRouter()
   return (
     <Layout>
-      <div className="flex flex-col items-center justify-center w-full h-full space-y-12 dark:text-white">
-        <img
+      <div className="flex flex-col items-center justify-center w-full mt-12 space-y-12 dark:text-white">
+        <Image
           src="/assets/travolta.webp"
+          height="500"
+          width="500"
           alt="Travola Lost"
-          className="flex-1 w-auto h-auto filter grayscale contrast-125 brightness-125 lg:mt-12"
+          className="flex-1 w-auto h-auto filter grayscale contrast-125 brightness-125"
         />
-        <p className="text-xl font-light text-gray-800 dark:text-gray-200">
+        <p className="max-w-xs text-xl font-light text-gray-800 md:max-w-max dark:text-gray-200">
           Whoops, it looks like the page you were looking for doesn&apos;t
           exist!
         </p>
         <button
           onClick={() => router.back()}
-          className="inline-flex items-center justify-center px-4 py-2 text-lg font-light text-gray-800 whitespace-no-wrap transition-all duration-300 ease-in-out bg-white border border-gray-300 rounded-md shadow-sm outline-none hover:bg-gray-50 focus:outline-none focus:shadow-none w-72 dark:bg-bgGray dark:border-palevioletred dark:text-gray-200 focus:ring-4 focus:ring-pink-300 dark:focus:ring-palevioletred dark:focus:ring-opacity-20 hover:ring-4 hover:ring-pink-300 dark:hover:ring-palevioletred dark:hover:ring-opacity-20"
+          className="inline-flex items-center justify-center px-4 py-2 text-lg font-light text-gray-800 whitespace-no-wrap transition-all duration-300 ease-in-out bg-white border border-gray-300 rounded-md shadow-sm outline-none hover:bg-gray-50 focus:outline-none focus:shadow-none w-80 dark:bg-bgGray dark:border-palevioletred dark:text-gray-200 focus:ring-4 focus:ring-pink-300 dark:focus:ring-palevioletred dark:focus:ring-opacity-20 hover:ring-4 hover:ring-pink-300 dark:hover:ring-palevioletred dark:hover:ring-opacity-20"
         >
           Back
         </button>
