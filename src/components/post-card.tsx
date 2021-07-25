4ece49965bde1a6c8bc4ac001570cd93aa0453a2
ndom91 <yo@ndo.dev>
2021-07-26 01:44:09 +0200

    fix: cleanup old postcard layout :broom:

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -123,66 +123,6 @@ const PostCard = ({ post, index }: Props) => {
       </div>
     </div>
   )
-
-  return (
-    <div className="mx-2 md:mx-8">
-      <div className="relative m-4">
-        <div className="relative flex flex-col transition duration-500 md:flex-row rounded-xl hover:shadow-sm-smooth">
-          <Link href={`/posts/${slug ? slug : ""}`}>
-            <a className="w-full mb-4 bg-gray-200 md:mb-0 md:w-44 dark:bg-gray-600 rounded-xl md:rounded-r-none hover:cursor-pointer post-card-image-wrapper">
-              {imageImport && (
-                <Image
-                  src={imageImport}
-                  alt={`${title} Cover Image`}
-                  quality="100"
-                  layout="responsive"
-                  placeholder="blur"
-                  objectFit="contain"
-                  className="rounded-xl md:rounded-r-none"
-                />
-              )}
-            </a>
-          </Link>
-
-          <div className="flex flex-col flex-1 p-1 md:p-10">
-            <div className="relative flex flex-col justify-between w-full md:flex-row">
-              <Link href={`/posts/${slug ? slug : ""}`}>
-                <a>
-                  <div className="text-xl font-medium text-gray-600 capitalize transition-colors duration-300 dark:text-gray-100 hover:text-palevioletred">
-                    {title}
-                  </div>
-                </a>
-              </Link>
-              <div className="flex flex-col-reverse text-sm md:flex-row md:text-base">
-                <span className="font-light text-gray-400 md:mx-4 dark:text-gray-900 min-w-max">
-                  {time.text}
-                </span>
-                <span className="text-medium min-w-max">
-                  <DateFormatter dateString={date} />
-                </span>
-              </div>
-            </div>
-            <div
-              className="flex w-full max-w-full mt-4 mb-4 prose-sm prose text-gray-600 dark:text-gray-200 prose-pink dark:prose-dark"
-              dangerouslySetInnerHTML={{ __html: excerpt }}
-            />
-            <div className="relative flex align-middle">
-              <div className="relative flex justify-start overflow-hidden">
-                {tags.map((tag) => (
-                  <span
-                    key={tag}
-                    className="px-2 py-1 my-auto mr-2 text-xs text-gray-400 bg-gray-200 rounded-full whitespace-nowrap dark:bg-gray-800 dark:text-palevioletred"
-                  >
-                    {tag}
-                  </span>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
 }
 
 export default PostCard
