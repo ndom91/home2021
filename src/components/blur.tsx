82eededa7cadabed6a76227fcdaca7b81d8c4faa
ndom91 <yo@ndo.dev>
2021-11-13 18:39:08 +0100

    style: dynamic homepage content margin

--- src/components/blur.tsx
+++ src/components/blur.tsx
@@ -1,6 +1,6 @@
 const Blur = () => {
   return (
-    <div className="absolute w-2/3 h-96 top-24 right-4 md:right-24">
+    <div className="absolute w-2/3 h-96 top-[15vh] right-4 md:right-24">
       <div className="absolute top-0 bg-purple-300 rounded-full right-4 w-72 h-72 opacity-60 dark:opacity-90 animate-blob blur-2xl filter mix-blend-multiply dark:mix-blend-overlay"></div>
       <div className="absolute top-0 bg-yellow-300 rounded-full opacity-50 animation-delay-2000 right-64 w-72 h-72 dark:opacity-80 animate-blob blur-2xl filter mix-blend-multiply dark:mix-blend-overlay"></div>
       <div className="absolute bg-pink-300 rounded-full opacity-50 animation-delay-4000 -bottom-4 right-32 w-72 h-72 dark:opacity-90 animate-blob blur-2xl filter mix-blend-multiply dark:mix-blend-overlay"></div>
--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -11,7 +11,7 @@ const Intro = () => {
   }, [])
 
   return (
-    <section className="relative z-10 flex flex-col items-start my-16 text-left lg:flex-row lg:items-center lg:justify-between lg:mb-12 lg:my-32">
+    <section className="relative z-10 flex flex-col items-start my-16 text-left lg:flex-row lg:items-center lg:justify-between lg:my-[20vh]">
       <div className="relative w-2/3 h-16">
         <h1 className="pl-4 font-bold leading-tight tracking-tighter opacity-0 js-darkmode-flicker text-7xl intro-title dark:text-gray-100 animate-fade-in-up-30 lg:pl-0 lg:pr-8 lg:text-8xl">
           ndom91.
