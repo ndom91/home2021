73e76344881a5b50e4da4cb957fec6d4bb6fc0c4
ndom91 <yo@ndo.dev>
2022-10-23 19:46:16 +0200

    fix: supabase-comments styling

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -95,7 +95,7 @@ const Post = ({ source, frontMatter, slug }: Props) => {
           <div className="prose prose-lg mx-auto max-w-4xl dark:prose-dark dark:text-gray-100">
             <MDXRemote {...source} components={components} lazy />
           </div>
-          <div className="not-prose prose prose-sm mx-auto mt-20 max-w-full dark:prose-dark dark:text-gray-100 md:prose-lg">
+          <div className="prose prose-sm mx-auto mt-20 max-w-full dark:prose-dark dark:text-gray-100 md:prose-lg">
             <CommentsProvider
               supabaseClient={supabase}
               onAuthRequested={() => setModalVisible(true)}
