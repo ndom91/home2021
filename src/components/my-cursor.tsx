fcc12a9b8b364fac37665a05d6b51d666ea1e30e
ndom91 <yo@ndo.dev>
2024-01-20 19:04:45 +0100

    feat: extract rendering custom cursors to own file

--- src/components/my-cursor.tsx
+++ src/components/my-cursor.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import useStore from '../lib/zustand'
+import { useEffect, useState } from "react"
+import useStore from "../lib/zustand"
 
 type CursorProps = {
   cursor: {
@@ -9,79 +9,81 @@ type CursorProps = {
 }
 
 const MyCursor = ({ cursor }: CursorProps) => {
-  const { x, y } = cursor
-  const [hover, setHover] = useState(false)
   const theme = useStore((state) => state.theme)
-  const hoverText = useStore((state) => state.hoverText)
+  const [isHovering, setIsHovering] = useState(false)
+  const isHoveringText = useStore((state) => state.hoverText)
 
   useEffect(() => {
     const handleMouseMove = () => {
-      setHover(true)
+      setIsHovering(true)
     }
     const handleMouseLeave = () => {
-      setHover(false)
+      setIsHovering(false)
     }
-    const el = document.querySelectorAll('a, button')
+    const el = document.querySelectorAll("a, button")
     el.forEach((link) => {
-      link.addEventListener('mouseover', handleMouseMove)
-      link.addEventListener('mouseleave', handleMouseLeave)
+      link.addEventListener("mouseover", handleMouseMove)
+      link.addEventListener("mouseleave", handleMouseLeave)
     })
+
     return () => {
       el.forEach((link) => {
-        link.removeEventListener('mouseover', handleMouseMove)
-        link.removeEventListener('mouseleave', handleMouseLeave)
+        link.removeEventListener("mouseover", handleMouseMove)
+        link.removeEventListener("mouseleave", handleMouseLeave)
       })
     }
   }, [])
 
   const backgroundColor = () => {
-    if (hoverText) return 'red'
-    if (hover) {
-      return theme === 'dark' ? 'rgba(200,200,255,.1)' : 'rgba(50,50,50,0.1)'
+    if (isHoveringText) return "red"
+    if (isHovering) {
+      return theme === "dark" ? "rgba(200,200,255,.1)" : "rgba(50,50,50,0.1)"
     } else {
-      return theme === 'dark' ? 'rgba(200,200,255,.2)' : 'rgba(50,50,50,0.4)'
+      return theme === "dark" ? "rgba(200,200,255,.2)" : "rgba(50,50,50,0.4)"
     }
   }
 
+  console.log({ x: cursor.x, y: cursor.y })
+
   return (
     <>
-      {hoverText ? (
+      {isHoveringText ? (
         <div
-          className="absolute hidden bg-pink-300 md:block"
+          className="hidden absolute bg-pink-300 md:block"
+          data-label="thin-text-cursor"
           style={{
-            top: `${y}px`,
-            left: `${x}px`,
+            top: `${cursor.y}px`,
+            left: `${cursor.x}px`,
             zIndex: 999,
-            borderRadius: '2px',
-            height: '36px',
-            pointerEvents: 'none',
-            position: 'absolute',
-            transform: 'translate(-50%,-50%)',
-            transitionDuration: '250ms',
-            transitionProperty:
-              'width,height,border-radius,transform,background',
-            transitionTimingFunction: 'cubic-bezier(0.68, -0.6, 0.22, 2.1)',
-            width: '3px',
+            borderRadius: "2px",
+            height: "36px",
+            pointerEvents: "none",
+            position: "absolute",
+            transform: "translate(-50%,-50%)",
+            transitionDuration: "250ms",
+            transitionProperty: "width,height,border-radius,transform,background",
+            transitionTimingFunction: "cubic-bezier(0.68, -0.6, 0.22, 2.1)",
+            width: "3px",
           }}
         />
       ) : (
         <div
-          className="absolute hidden md:block"
+          className="hidden absolute md:block"
+          data-label="round-cursor"
           style={{
-            top: `${y}px`,
-            left: `${x}px`,
+            top: `${cursor.y}px`,
+            left: `${cursor.x}px`,
             zIndex: 999,
             background: backgroundColor(),
-            borderRadius: hover ? '16px' : '100%',
-            height: hover ? '48px' : '36px',
-            pointerEvents: 'none',
-            position: 'absolute',
-            transform: 'translate(-50%,-50%)',
-            transitionDuration: '250ms',
-            transitionProperty:
-              'width,height,border-radius,transform,background',
-            transitionTimingFunction: 'cubic-bezier(0.68, -0.6, 0.22, 2.1)',
-            width: hover ? '48px' : '36px',
+            borderRadius: isHovering ? "8px" : "100%",
+            height: isHovering ? "48px" : "36px",
+            pointerEvents: "none",
+            position: "absolute",
+            transform: "translate(-50%,-50%)",
+            transitionDuration: "250ms",
+            transitionProperty: "width,height,border-radius,transform,background",
+            transitionTimingFunction: "cubic-bezier(0.68, -0.6, 0.22, 2.1)",
+            width: isHovering ? "48px" : "36px",
           }}
         />
       )}
