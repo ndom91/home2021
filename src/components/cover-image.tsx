4ff3b6a57bf7bcf443ec026d36440a4e5e5bffd5
ndom91 <yo@ndo.dev>
2024-01-20 15:15:26 +0100

    fix: significantly simplify cover-image component

--- src/components/cover-image.tsx
+++ src/components/cover-image.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link"
+import Image from "next/image"
 
 type Props = {
   title: string
@@ -10,70 +9,31 @@ type Props = {
   slug?: string
 }
 
-const CoverImage = ({ title, cover = { imageFile: '' }, slug }: Props) => {
-  const { imageFile } = cover
-
-  const [imageImport, setImage] = useState('')
-  const loadImage = (imgur: string) => {
-    if (!imgur) {
-      return false
-    }
-    import(`../../public/assets/blog/${imgur}`).then((image) => {
-      setImage(image)
-    })
-  }
-  loadImage(imageFile)
-
-  const imageComponent = !imageImport ? (
-    <div className="relative py-4 my-4 font-mono font-extralight text-center text-gray-200 dark:text-gray-700">
-      Loading..
-    </div>
-  ) : (
-    <Image
-      src={imageImport}
-      alt={`Cover Image for ${title}`}
-      priority
-      quality="100"
-      layout="responsive"
-      objectFit="fill"
-      placeholder="blur"
-      className={
-        slug
-          ? 'rounded-lg shadow-sm transition-shadow duration-200 hover:shadow-md'
-          : 'rounded-lg bg-blend-overlay shadow-sm'
-      }
-    />
-  )
-
+const CoverImage = ({ title, cover, slug }: Props) => {
   return (
     <div className="sm:mx-0">
-      {!imageFile.includes('.gif') ? (
-        <Link
-          as={slug ? `/posts/${slug}` : '#'}
-          href="/posts/[slug]"
-          aria-label={title}
-          className="bg-pink-300"
-          tabIndex={-1}
-        >
-          {imageComponent}
-        </Link>
-      ) : (
-        <Link
-          as={slug ? `/posts/${slug}` : '#'}
-          href="/posts/[slug]"
-          aria-label={title}
-          className="bg-pink-300"
-          tabIndex={-1}
-        >
-          {/* eslint-disable @next/next/no-img-element */}
-          <img
-            alt={slug}
-            src={`/assets/blog/${imageFile}`}
-            className="mx-auto"
-            loading="eager"
-          />
-        </Link>
-      )}
+      <Link
+        as={slug ? `/posts/${slug}` : "#"}
+        href="/posts/[slug]"
+        aria-label={title}
+        className="bg-pink-300"
+        tabIndex={-1}
+      >
+        <Image
+          src={`/assets/blog/${cover.imageFile}`}
+          alt={`Cover Image for ${title}`}
+          priority
+          layout="responsive"
+          width={100}
+          height={100}
+          objectFit="fill"
+          className={
+            slug
+              ? "rounded-lg shadow-sm transition-shadow duration-200 hover:shadow-md"
+              : "rounded-lg bg-blend-overlay shadow-sm"
+          }
+        />
+      </Link>
     </div>
   )
 }
