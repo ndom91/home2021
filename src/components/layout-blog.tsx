f9c3be22e3f8d89d7a9b23202544ec5c0f6e224e
ndom91 <yo@ndo.dev>
2024-01-20 17:46:01 +0100

    feat: make post a bit more readable

--- src/components/layout-blog.tsx
+++ src/components/layout-blog.tsx
@@ -36,7 +36,7 @@ const Layout = ({ children }: Props) => {
       {alert.enabled && !value?.dismissed && (
         <Alert header={alert.header} body={alert.body} setValue={setValue} />
       )}
-      <div className="flex flex-col justify-center items-center px-4 w-full max-w-full min-h-screen md:overflow-x-visible md:px-8 lg:py-4 md:max-w-10xl">
+      <div className="flex flex-col justify-center items-center px-4 w-full max-w-full min-h-screen bg-white md:overflow-x-visible md:px-8 lg:py-4 dark:bg-gray-900 md:max-w-8xl">
         <Navbar />
         <main className="flex relative flex-col flex-1 items-center max-w-full">{children}</main>
         <Footer />
--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -1,7 +1,7 @@
-import DateFormatter from '@/components/date-formatter'
-import { Parallax, ParallaxProvider } from 'react-scroll-parallax'
-import CoverImage from '@/components/cover-image'
-import Link from 'next/link'
+import DateFormatter from "@/components/date-formatter"
+import { Parallax, ParallaxProvider } from "react-scroll-parallax"
+import CoverImage from "@/components/cover-image"
+import Link from "next/link"
 
 type Props = {
   post: {
@@ -27,41 +27,39 @@ const PostCard = ({ post }: Props) => {
   const { title, date, excerpt, slug, tags, time, cover } = post
 
   return (
-    <div className="relative w-full max-w-full flex-col rounded-2xl bg-white drop-shadow-lg dark:bg-gray-700 md:my-4 md:inline-flex md:max-w-md">
+    <div className="relative flex-col w-full max-w-full bg-white rounded-2xl md:inline-flex md:my-4 md:max-w-md dark:bg-gray-700 drop-shadow-lg">
       <div className="z-20 p-10">
-        <div className="mask-bottom-strong z-20 mb-6 h-auto overflow-hidden rounded-t-lg md:mb-6">
+        <div className="overflow-hidden z-20 mb-6 h-auto rounded-t-lg md:mb-6 mask-bottom-strong">
           <ParallaxProvider>
             <Parallax translateY={[0, -10]}>
               <CoverImage title={title} cover={cover} slug={slug} />
             </Parallax>
           </ParallaxProvider>
         </div>
-        <div className="relative z-20 flex w-full flex-col justify-between">
+        <div className="flex relative z-20 flex-col justify-between w-full">
           <Link
-            href={`/posts/${slug ? slug : ''}`}
-            className="rounded-sm text-2xl font-extralight text-gray-600 outline-none transition-colors duration-300 hover:text-palevioletred hover:outline-none focus:outline-none focus:ring-4 focus:ring-pink-300 focus:ring-offset-2 focus:ring-offset-white dark:text-gray-100 focus:dark:ring-palevioletred focus:dark:ring-offset-gray-500 md:text-2xl"
+            href={`/posts/${slug ? slug : ""}`}
+            className="text-2xl font-extralight text-gray-600 rounded-sm transition-colors duration-300 outline-none md:text-2xl dark:text-gray-100 hover:outline-none focus:ring-4 focus:ring-pink-300 focus:ring-offset-2 focus:ring-offset-white focus:outline-none hover:text-palevioletred focus:dark:ring-palevioletred focus:dark:ring-offset-gray-500"
           >
             {title}
           </Link>
-          <div className="flex flex-col font-block text-sm font-light items-start md:text-base space-y-1">
+          <div className="flex flex-col items-start space-y-1 text-sm font-light md:text-base">
             <span className="text-medium">
               <DateFormatter dateString={date} />
             </span>
-            <span className="text-gray-400 dark:text-gray-200">
-              {time.text}
-            </span>
+            <span className="text-gray-400 dark:text-gray-200">{time.text}</span>
           </div>
         </div>
         <div
-          className="prose-md prose prose-pink z-20 mt-4 flex w-full max-w-full font-light text-gray-600 dark:prose-dark dark:text-gray-200 line-clamp-4"
+          className="flex z-20 mt-4 w-full max-w-full font-light text-gray-600 dark:text-gray-200 prose-md prose prose-pink card-description dark:prose-dark"
           dangerouslySetInnerHTML={{ __html: excerpt }}
         />
-        <div className="relative z-20 mt-6 flex align-middle">
-          <div className="relative flex flex-wrap justify-start overflow-hidden">
+        <div className="flex relative z-20 mt-6 align-middle">
+          <div className="flex overflow-hidden relative flex-wrap justify-start">
             {tags.map((tag) => (
               <span
                 key={tag}
-                className="my-2 mr-2 whitespace-nowrap rounded-full bg-gray-100 px-4 py-2 font-block text-sm font-medium text-palevioletred dark:bg-pink-200 dark:text-palevioletred"
+                className="py-2 px-4 my-2 mr-2 text-sm font-medium whitespace-nowrap bg-gray-100 rounded-full dark:text-pink-800 dark:bg-pink-200 font-block text-palevioletred"
               >
                 {tag}
               </span>
