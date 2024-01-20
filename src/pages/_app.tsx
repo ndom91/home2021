e5d7293fc2489879b7257f4ecf3cc4ae649cf193
ndom91 <yo@ndo.dev>
2024-01-20 19:03:41 +0100

    feat: move Inter to use next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from "next/app"
+import { Inter } from "next/font/google"
 import "../styles/index.css"
 import "../styles/prism-coldark-dark.css"
 import "../styles/prism-coldark-light.css"
 
+const inter = Inter({ weight: ["100", "300", "900"], display: "swap", subsets: ["latin"] })
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
