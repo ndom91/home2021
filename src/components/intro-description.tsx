37c2f594bfcade1bba9c21c42e2761de4f802745
ndom91 <yo@ndo.dev>
2024-01-20 15:14:45 +0100

    fix: update intro description and add resume pdf

--- src/components/intro-description.tsx
+++ src/components/intro-description.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { useEffect } from "react"
 import Splitting from "splitting"
 import ScreenshotLink from "@/components/screenshot-link"
@@ -19,13 +20,16 @@ const IntroDescription = () => {
       className="relative p-2 mt-4 w-full font-mono text-lg font-extralight tracking-tight leading-8 text-left text-gray-800 md:text-xl lg:pl-10 lg:w-11/12 dark:text-gray-100"
       style={{ lineHeight: "2.25rem" }}
     >
-      Hi! My name&apos;s Nico Domino and I&lsquo;m a full-stack javascript developer at{" "}
-      <ScreenshotLink
-        url="https://checklyhq.com?utm_source=ndom91&utm_medium=referral"
-        text="Checkly"
-        className="opacity-0 animation-delay-500 animate-fade_in_up"
-      />{" "}
-      🦝. In the evenings you can find me working on open source software, like{" "}
+      Hey visitor 👋 My name&apos;s Nico Domino and I&lsquo;m a senior full-stack javascript
+      developer currently looking for{" "}
+      <Link
+        href="/assets/ndomino_webdev_180124.pdf"
+        target="_blank"
+        className="transition-all outline-none border-underline-grow dark:ring-palevioletred"
+      >
+        a new gig
+      </Link>
+      . In the evenings you can find me working on open source software, like{" "}
       <ScreenshotLink
         url="https://next-auth.js.org?utm_source=ndom91"
         text="Auth.js"
