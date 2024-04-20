9a899a7d11ed584c47b9d48f15b00022fa98e9c0
ndom91 <yo@ndo.dev>
2024-04-20 16:31:28 +0200

    fix: screenshot link in posts

--- src/components/screenshot-link.tsx
+++ src/components/screenshot-link.tsx
@@ -29,9 +29,9 @@ const ScreenshotLink = ({ url, image, children }: ScreenshotLinkProps) => {
           // @ts-expect-error setting css variable
           "--image_rotation": `${randomInteger(-8, 8)}deg`,
         }}
-        className="hidden opacity-0 absolute bottom-16 right-1/2 z-20 p-1 w-32 rounded-lg border transition ease-[var(--ease-spring-5)] rotate-6 pointer-events-none group-focus:block group-hover:block border-gray-200/50 animate-fade_in_up_5_rotate bg-gray-100/50 backdrop-blur-xl dark:bg-gray-300/10 dark:border-gray-400/50"
+        className="hidden opacity-0 absolute bottom-16 right-1/2 z-20 p-1 w-32 rounded-lg border transition ease-[var(--ease-spring-5)] pointer-events-none group-focus:block group-hover:block border-gray-200/50 animate-fade_in_up_5_rotate bg-gray-100/50 backdrop-blur-xl dark:bg-gray-300/10 dark:border-gray-400/50"
       >
-        <img src={`/screenshots/${image}`} className="rounded-md" />
+        <img src={`/screenshots/${image}`} className="rounded-md !m-0" />
       </div>
       <span className="">{children}</span>
     </a>
