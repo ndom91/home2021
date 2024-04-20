0f11a9c3f9ecc879433900ea44d7246842570950
ndom91 <yo@ndo.dev>
2024-04-20 17:58:53 +0200

    fix: favicon

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -14,11 +14,12 @@ const Meta = () => {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
-        <link rel="shortcut icon" href="/logo.svg" />
-        <meta name="msapplication-TileColor" content="#000000" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#d9d9d9" />
         <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-        <meta name="theme-color" content="#FCE7F3" />
+        <meta name="theme-color" content="#ffffff" />
+
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
         <meta name="description" content={`ndom91`} />
 
