8a9fe921fc0c86cca242619a7b561f939a657962
ndom91 <yo@ndo.dev>
2024-05-15 13:52:08 +0200

    fix: blog post mask

--- src/components/mask-post.tsx
+++ src/components/mask-post.tsx
@@ -1,35 +1,37 @@
 export default function Mask() {
   return (
-    <svg
-      className="fixed top-0 left-1/2 transition -translate-x-1/2 pointer-events-none duration-0"
-      style={{ maskRepeat: "repeat" }}
-      viewBox="0 0 800 800"
-      height="100%"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <defs>
-        <linearGradient id="lgradient" gradientTransform="rotate(0 .5 .5)">
-          <stop offset="0" stopColor="rgba(0,0,0,0)" />
-          <stop offset="0.1" stopColor="rgba(255,255,255,1)" />
-          <stop offset="0.9" stopColor="rgba(255,255,255,1)" />
-          <stop offset="1" stopColor="rgba(0,0,0,0)" />
-        </linearGradient>
-      </defs>
-      <mask id="my-mask">
-        <rect fill="url(#lgradient)" width="800" height="800"></rect>
-      </mask>
-
-      <polygon
-        points="0,0 800,0 800,800 0,800"
-        mask="url(#my-mask)"
-        x="0"
-        y="0"
-        width="100%"
+    <div className="fixed top-0 left-1/2 w-full h-full transition -translate-x-1/2 pointer-events-none duration-0">
+      <svg
+        preserveAspectRatio="none"
+        viewBox="0 0 800 800"
         height="100%"
-        fillOpacity="0.8"
-        fill="currentColor"
-        className="text-white dark:text-gray-900/80"
-      />
-    </svg>
+        width="100%"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <defs>
+          <linearGradient id="lgradient" gradientTransform="rotate(0 .5 .5)">
+            <stop offset="0" stopColor="rgba(0,0,0,0)" />
+            <stop offset="0.1" stopColor="rgba(255,255,255,1)" />
+            <stop offset="0.9" stopColor="rgba(255,255,255,1)" />
+            <stop offset="1" stopColor="rgba(0,0,0,0)" />
+          </linearGradient>
+        </defs>
+        <mask id="my-mask">
+          <rect fill="url(#lgradient)" width="800" height="800"></rect>
+        </mask>
+
+        <polygon
+          points="0,0 800,0 800,800 0,800"
+          mask="url(#my-mask)"
+          x="0"
+          y="0"
+          width="100%"
+          height="100%"
+          fillOpacity="0.8"
+          fill="currentColor"
+          className="text-white dark:text-gray-900/80"
+        />
+      </svg>
+    </div>
   )
 }
