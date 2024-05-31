b0d7cca482c5e1fed537ba44e38398e4dd6c5772
ndom91 <yo@ndo.dev>
2024-05-31 13:51:13 +0200

    fix: cleanup hero section

--- src/components/hero-desc.tsx
+++ src/components/hero-desc.tsx
@@ -5,15 +5,8 @@ const IntroDescription = () => (
     className="relative p-2 mt-4 w-full text-lg font-thin text-left text-gray-800 md:text-xl lg:pl-10 lg:w-11/12 dark:text-gray-100 font-mono leading-[2.75rem] animate-[var(--animation-fade-in-bloom)] text-balance"
     style={{ animationDuration: "500ms" }}
   >
-    Hey visitor 👋 My name&apos;s Nico Domino and I&lsquo;m a senior fullstack web developer
-    currently looking for{" "}
-    <ScreenshotLink
-      url="https://cv.ndo.dev/fullstack?utm_source=portfolio&utm_medium=description&utm_campaign=resume"
-      image="resume.jpg"
-    >
-      a new gig
-    </ScreenshotLink>{" "}
-    📝. You can also find me working on open source software, like{" "}
+    Hey visitor 👋 My name&apos;s Nico Domino and I&lsquo;m a senior fullstack web developer. You
+    can find me working on open source software, like{" "}
     <ScreenshotLink url="https://authjs.dev" image="authjs.jpg">
       Auth.js
     </ScreenshotLink>{" "}
