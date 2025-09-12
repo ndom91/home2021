f8b27178242ffdf99597c821d549085851bce762
ndom91 <yo@ndo.dev>
2025-09-12 18:24:46 +0200

    Update avatar

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,18 +1,18 @@
-import Image from "next/image"
+import Image from "next/image";
 
 const Avatar = () => {
-  return (
-    <div className="flex flex-col items-center">
-      <Image
-        src="/assets/img/brick-avatar.webp"
-        quality="100"
-        height="128"
-        width="128"
-        className="mr-4 rounded-full !size-14 sm:!size-28"
-        alt="ndom91"
-      />
-    </div>
-  )
-}
+	return (
+		<div className="flex flex-col items-center">
+			<Image
+				src="/assets/img/notion-avatar.png"
+				quality="100"
+				height="128"
+				width="128"
+				className="mr-4 rounded-full !size-14 sm:!size-28"
+				alt="ndom91"
+			/>
+		</div>
+	);
+};
 
-export default Avatar
+export default Avatar;
